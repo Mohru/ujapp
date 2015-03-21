047bf09ac81f5f93e53398caac2d435a2d48d2a0
Aleksandra Majchrzak <aleksandra.a.majchrzak@gmail.com>
2015-03-21 18:50:25 +0000

    Did some layout changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,42 +7,43 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-  @article = Article.find(params[:id])
-end
-  
-def create
-   @article = Article.new(article_params)
- 
-  if @article.save
-    redirect_to @article
-  else
-    render 'new'
+    @article = Article.find(params[:id])
   end
-end
+  
+  def create
+    @article = Article.new(article_params)
+ 
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
   
   def show
     @article = Article.find(params[:id])
   end
  
-    def index
+  def index
     @articles = Article.all
   end
   
   def update
-  @article = Article.find(params[:id])
+    @article = Article.find(params[:id])
  
-  if @article.update(article_params)
-    redirect_to @article
-  else
-    render 'edit'
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
-end
+  
   def destroy
-  @article = Article.find(params[:id])
-  @article.destroy
+    @article = Article.find(params[:id])
+    @article.destroy
  
-  redirect_to articles_path
-end
+    redirect_to articles_path
+  end
   
 private
   def article_params
