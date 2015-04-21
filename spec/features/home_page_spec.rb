f71e991e51aee133066ef2beaba511177dca9abb
Aleksandra Majchrzak <aleksandra.a.majchrzak@gmail.com>
2015-04-21 12:56:53 +0000

    Add feature test for home page.

---
+++ spec/features/home_page_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'home page' do
+  describe 'user visits home page', type: :feature do
+    it 'user sees home page' do
+      visit '/'
+      
+      expect(page). to have_content('Welcome!')
+    end
+  end
+end
