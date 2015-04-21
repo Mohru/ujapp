9db7a63cb581941c2cdebfcbed89c0119d127e7c
Aleksandra Majchrzak <aleksandra.a.majchrzak@gmail.com>
2015-04-21 12:20:42 +0000

    Add test for article show.

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,13 +1,25 @@
 require 'rails_helper'
 
 describe ArticlesController do
+  before :each @article=create(:article)
+  
   describe 'index' do
     it 'returns articles list' do
-      article=create(:article)
+      #article=create(:article)
       get :index
       
       expect(response).to have_http_status(:success)
       expect(assigns(:articles).length).to eq 1
    end
+  end
+  
+  describe 'show' do
+    it 'shows single article' do
+      get :show, id: @article.id
+      
+      expect(response).to have_http_status(:success)
+      expect(assigns(:article)).not_to eq nil
+      expect(assigns(:articles).title).to eq @article.title
+    end
   end
 end
