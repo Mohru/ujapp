9e8ff30fb35c8810e18834ae92fd0a85070da1a8
Aleksandra Majchrzak <aleksandra.a.majchrzak@gmail.com>
2015-04-21 13:27:28 +0000

    Add feature test for creating new article when not signed in.

--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -14,6 +14,16 @@ feature 'articles' do
       expect(page).to have_content(@second_article.title)
       expect(page).to have_content('© Company 2015')
     end
+    
+    it 'user creates a new article' do
+      visit articles_path
+      
+      click_link('New article')
+      
+      expect(page).to have_content('You need to sign in or sign up before continuing.')
+      expect(page).not_to have_content(@article.title)
+      expect(page.current_url).to eq 'http://www.example.com/users/sign_in'
+    end
   end
 end
 
